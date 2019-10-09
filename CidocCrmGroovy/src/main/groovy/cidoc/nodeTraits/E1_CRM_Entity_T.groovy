aa9cbb02c2e1a1ea4f378dbf2ce2f5f72855eb2e
João Rocha da Silva <joaorosilva@gmail.com>
2019-10-09 17:46:42 +0100

    Merge branch 'orientdb-experiment' of https://github.com/feup-infolab/archgraph into orientdb-experiment
    # Conflicts:
    #	CidocCrmGroovy/.gradle/4.8/fileContent/annotation-processors.bin
    #	CidocCrmGroovy/.gradle/4.8/fileContent/fileContent.lock
    #	CidocCrmGroovy/.gradle/4.8/fileHashes/fileHashes.bin
    #	CidocCrmGroovy/.gradle/4.8/fileHashes/fileHashes.lock
    #	CidocCrmGroovy/.gradle/4.8/fileHashes/resourceHashesCache.bin
    #	CidocCrmGroovy/.gradle/4.8/taskHistory/taskHistory.bin
    #	CidocCrmGroovy/.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	CidocCrmGroovy/.gradle/buildOutputCleanup/cache.properties
    #	CidocCrmGroovy/.gradle/buildOutputCleanup/outputFiles.bin
    #	CidocCrmGroovy/src/ratpack/ratpack.groovy

--- CidocCrmGroovy/src/main/groovy/cidoc/nodeTraits/E1_CRM_Entity_T.groovy
+++ CidocCrmGroovy/src/main/groovy/cidoc/nodeTraits/E1_CRM_Entity_T.groovy
@@ -9,23 +9,17 @@ import lombok.Getter
 @CompileStatic
 trait E1_CRM_Entity_T {
     @Getter
-    static Object P1_is_identified_by_t = E41_Appellation_T.class
-    @Getter
-    static Object P2_has_type_t = E55_Type_T.class
-    @Getter
-    static Object AP12_has_level_of_description_t = E55_Type_T.class
-
     private ArrayList<E41_Appellation_T> P1_is_identified_by = new ArrayList<>()
+    @Getter
     private ArrayList<E55_Type_T> P2_has_type = new ArrayList<>()
+    @Getter
     private ArrayList<E55_Type_T> AP12_has_level_of_description = new ArrayList<>()
 
     Xsd_String P3_has_note
 
     static mapping = {
-        P1_is_identified_by(edge: P1_is_identified_by_t)
-        P2_has_type(edge: P2_has_type_t)
-        AP12_has_level_of_description(edge: AP12_has_level_of_description_t)
+        P1_is_identified_by(edge: E41_Appellation_T)
+        P2_has_type(edge: E55_Type_T)
+        AP12_has_level_of_description(edge: E55_Type_T)
     }
-
-
 }
