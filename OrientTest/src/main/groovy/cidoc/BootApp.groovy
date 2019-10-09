f1cfe27565da51ee80138f9bca499b54d904c078
Nuno <nuno.freitas96@gmail.com>
2019-10-09 14:50:22 +0100

    Commit if necessary to regress

---
+++ OrientTest/src/main/groovy/cidoc/BootApp.groovy
@@ -0,0 +1,49 @@
+package cidoc;
+
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+import groovy.transform.CompileStatic;
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Bean
+import org.springframework.context.event.ContextRefreshedEvent
+
+@CompileStatic
+@EnableAutoConfiguration
+class BootApp extends SpringBootServletInitializer implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    OrientGraphFactory graphFactory
+
+    /**
+     *  Create entities on startup
+     * @param event
+     */
+    @Override
+    void onApplicationEvent(ContextRefreshedEvent event) {
+        graphFactory.withTransaction {
+
+        }
+    }
+
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        application.sources BootApp
+    }
+
+    static void main(String[] args) {
+        SpringApplication.run BootApp, args
+    }
+
+    @Bean
+    public OrientGraphFactory databaseFactory() {
+        // change to 'remote:host/dbname' if persistent storage needed
+        def factory = new OrientGraphFactory("memory:test").setupPool(1, 20)
+        factory
+    }
+}
