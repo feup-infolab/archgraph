2a8e9bf2732a93b00b4a2cc5132538775754e5be
João Rocha da Silva <joaorosilva@gmail.com>
2019-10-15 16:37:15 +0100

    more code experiments.

--- CidocCrmGroovy/src/main/groovy/com/github/feupinfolab/archgraph/cidoc/nodeTraits/E1_CRM_Entity_T.groovy
+++ CidocCrmGroovy/src/main/groovy/com/github/feupinfolab/archgraph/cidoc/nodeTraits/E1_CRM_Entity_T.groovy
@@ -4,6 +4,7 @@ import com.github.eugene.kamenev.orient.graph.Vertex
 import com.github.feupinfolab.archgraph.cidoc.edges.AP12_has_level_of_description
 import com.github.feupinfolab.archgraph.cidoc.edges.P1_is_identified_by
 import com.github.feupinfolab.archgraph.cidoc.edges.P2_has_type
+import com.github.feupinfolab.archgraph.cidoc.nodes.E55_Type
 import com.github.feupinfolab.archgraph.dataobject.Xsd_String
 import groovy.transform.CompileStatic
 import lombok.Getter
@@ -17,14 +18,14 @@ trait E1_CRM_Entity_T {
     String name
 
     List<E41_Appellation_T> isIdentifiedBy
-    List<E55_Type_T> hasType
+    List<E55_Type> hasType
     List<E55_Type_T> hasLevelOfDescription
 
     Xsd_String P3_has_note
 
     static mapping = {
-        hasType(edge: P2_has_type)
-        isIdentifiedBy(edge: P1_is_identified_by)
-        hasLevelOfDescription(edge: AP12_has_level_of_description)
+        hasType edge: P2_has_type
+        isIdentifiedBy edge: P1_is_identified_by
+        hasLevelOfDescription edge: AP12_has_level_of_description
     }
 }
