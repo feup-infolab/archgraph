82eaeb72c1531ffd626b079917cf941f48e8a78c
Nuno <nuno.freitas96@gmail.com>
2019-10-18 14:54:53 +0100

    OrientDB functionalities start

--- TestFerma/src/main/groovy/Loader.groovy
+++ TestFerma/src/main/groovy/Loader.groovy
@@ -1,17 +1,21 @@
 import com.syncleus.ferma.DefaultClassInitializer
 import com.syncleus.ferma.DelegatingFramedGraph
 import com.syncleus.ferma.FramedGraph
-
+import com.syncleus.ferma.ext.orientdb.impl.OrientTransactionFactoryImpl
+import com.syncleus.ferma.tx.TxFactory
 import edges.P2_has_type
 import groovy.transform.CompileStatic
 import nodeTraits.E1_CRM_Entity_T
 import nodeTraits.E55_Type_T
+import org.apache.tinkerpop.gremlin.orientdb.OrientGraph
+import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
 import org.apache.tinkerpop.gremlin.process.traversal.Traverser
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 import org.apache.tinkerpop.gremlin.process.traversal.step.filter.HasStep
 import org.apache.tinkerpop.gremlin.structure.Graph
 import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
 
+@CompileStatic
 class Loader {
 
     public static void testAnnotatedTyping() {
@@ -35,6 +39,23 @@ class Loader {
         //List<E55_Type_T> e55Again = e1Again.getHasTypeE55()
 
         println(e1Again)
+        //println(e55Again)
+
+
+    }
+
+    public static void testOrientdbFerma() {
+
+
+        Set<Class<?>> types = new HashSet<Class<?>>(Arrays.asList(
+                E1_CRM_Entity_T.class,
+                E55_Type_T.class,
+                P2_has_type.class));
+
+        OrientGraphFactory graphFactory = new OrientGraphFactory("memory:tinkerpop")
+        TxFactory graph = new OrientTransactionFactoryImpl(graphFactory,false,"com.syncleus.ferma.ext.orientdb.model")
+
+
         //println(e55Again)
 
 
