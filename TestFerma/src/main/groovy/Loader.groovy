8ca722736c8823835b180693ede36c43128d2a8a
João Rocha da Silva <joaorosilva@gmail.com>
2019-10-18 18:42:21 +0100

    Trying to get this to run on Database and Memory. The code in the example is a mess, and does not compile in Groovy because of lambdas, unless you use Groovy 3 or convert everything to clojures.

--- TestFerma/src/main/groovy/Loader.groovy
+++ TestFerma/src/main/groovy/Loader.groovy
@@ -1,58 +1,64 @@
 import com.syncleus.ferma.DefaultClassInitializer
-import com.syncleus.ferma.DelegatingFramedGraph
-import com.syncleus.ferma.FramedGraph
 import com.syncleus.ferma.ext.orientdb.impl.OrientTransactionFactoryImpl
+import com.syncleus.ferma.tx.Tx
+import com.syncleus.ferma.tx.TxAction0
 import com.syncleus.ferma.tx.TxFactory
+import db.DBConnection
 import edges.P2_has_type
 import groovy.transform.CompileStatic
 import nodeTraits.E1_CRM_Entity_T
 import nodeTraits.E55_Type_T
-import org.apache.tinkerpop.gremlin.orientdb.OrientGraph
 import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
-import org.apache.tinkerpop.gremlin.process.traversal.Traverser
-import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
-import org.apache.tinkerpop.gremlin.process.traversal.step.filter.HasStep
-import org.apache.tinkerpop.gremlin.structure.Graph
-import org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph
 
 @CompileStatic
 class Loader {
 
-    public static void testAnnotatedTyping() {
-
+    static void testAnnotatedTyping() {
 
         Set<Class<?>> types = new HashSet<Class<?>>(Arrays.asList(
                 E1_CRM_Entity_T.class,
                 E55_Type_T.class,
-                P2_has_type.class));
+                P2_has_type.class))
 
-        Graph graph = TinkerGraph.open()
-        FramedGraph fd = new DelegatingFramedGraph(graph,true,types)
-        E1_CRM_Entity_T e1 = (E1_CRM_Entity_T) fd.addFramedVertex(E1_CRM_Entity_T.class as Class<Object>)
-        E55_Type_T e55 = (E55_Type_T) fd.addFramedVertex(E55_Type_T.class as Class<Object>)
-        e1.setName("E1")
-        e55.setName("E55")
-        e1.addHasType(e55, new DefaultClassInitializer(P2_has_type.class))
+        OrientGraphFactory graphFactory = DBConnection.memoryFactory()
 
-        GraphTraversalSource g = graph.traversal()
-        E1_CRM_Entity_T e1Again = fd.traverse({g.V().has("name", "E1") }).next(E1_CRM_Entity_T.class)
-        //List<E55_Type_T> e55Again = e1Again.getHasTypeE55()
+        TxFactory graph = new OrientTransactionFactoryImpl(graphFactory, false, "com.syncleus.ferma.ext.orientdb.model")
 
-        println(e1Again)
+        try {
+            Tx tx = graph.tx()
+            E1_CRM_Entity_T e1 = (E1_CRM_Entity_T) tx.getGraph().addFramedVertex(E1_CRM_Entity_T.class as Class<Object>)
+            E55_Type_T e55 = (E55_Type_T) tx.getGraph().addFramedVertex(E55_Type_T.class as Class<Object>)
+            e1.setName("E1")
+            e55.setName("E55")
+            e1.addHasType(e55, new DefaultClassInitializer(P2_has_type.class))
+            tx.success()
+        }
+        catch (Exception e)
+        {
+            println ("Deu asneira: " + e.getMessage())
+        }
+
+        try (Tx tx = graph.tx()) {
+            tx.getGraph().getFramedVerticesExplicit(E1_CRM_Entity_T.class).collect {
+                (entity) -> {
+                    println("Found E1_CRM_Entity with name: " + entity.getClass().getName())
+                }
+            }
+        }
+
         //println(e55Again)
-
-
+        //List<E55_Type_T> e55Again = e1Again.getHasTypeE55()
     }
 
-    public static void testOrientdbFerma() {
+    static void testOrientdbFerma() {
 
 
         Set<Class<?>> types = new HashSet<Class<?>>(Arrays.asList(
                 E1_CRM_Entity_T.class,
                 E55_Type_T.class,
-                P2_has_type.class));
+                P2_has_type.class))
 
-        OrientGraphFactory graphFactory = new OrientGraphFactory("memory:tinkerpop")
+        OrientGraphFactory graphFactory = DBConnection.databaseFactory()
         TxFactory graph = new OrientTransactionFactoryImpl(graphFactory,false,"com.syncleus.ferma.ext.orientdb.model")
 
 
@@ -61,7 +67,7 @@ class Loader {
 
     }
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         testAnnotatedTyping()
     }
 }
