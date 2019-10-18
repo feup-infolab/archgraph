82e1d11635b9ee1d0906ac0dfd844a2ed9c7f97d
João Rocha <joaorosilva@gmail.com>
2019-10-19 00:09:39 +0100

    Changes to docker-compose

--- TestFerma/src/main/groovy/db/DBConnection.groovy
+++ TestFerma/src/main/groovy/db/DBConnection.groovy
@@ -5,21 +5,21 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 class DBConnection {
-    OrientGraphFactory graphFactory
+	OrientGraphFactory graphFactory
 
-    public static final String DATABASE_URI = "remote:127.0.0.1/archgraph"
-    public static final String SERVER_USERNAME = "root"
-    public static final String SERVER_PASSWORD = "rootpwd"
+	public static final String DATABASE_URI = "remote:127.0.0.1/archgraph"
+	public static final String SERVER_USERNAME = "root"
+	public static final String SERVER_PASSWORD = "rootpwd"
 
-    static OrientGraphFactory databaseFactory() {
-        // change to 'remote:host/dbname' if persistent storage needed
-        def factory = new OrientGraphFactory(DATABASE_URI,SERVER_USERNAME, SERVER_PASSWORD).setupPool(1, 20)
-        factory.getDatabase(true, true)
-        factory
-    }
+	static OrientGraphFactory databaseFactory() {
+		// change to 'remote:host/dbname' if persistent storage needed
+		def factory = new OrientGraphFactory(DATABASE_URI,SERVER_USERNAME, SERVER_PASSWORD).setupPool(1, 20)
+		factory.getDatabase(true, true)
+		factory
+	}
 
-    static OrientGraphFactory memoryFactory(){
-        OrientGraphFactory factory = new OrientGraphFactory("memory:tinkerpop")
-        factory
-    }
+	static OrientGraphFactory memoryFactory(){
+		OrientGraphFactory factory = new OrientGraphFactory("memory:tinkerpop")
+		factory
+	}
 }
