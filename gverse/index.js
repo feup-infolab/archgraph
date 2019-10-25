638a4b95cdbb147d5ab708a9f72483cbed54037a
João Rocha da Silva <joaorosilva@gmail.com>
2019-10-25 15:48:14 +0100

    Added base gverse project

---
+++ gverse/index.js
@@ -0,0 +1,5 @@
+var sayHello = function (name) {
+    return "Hello, " + name + "!";
+};
+var user = [42, 9000, 7];
+console.log(sayHello(user));
---
+++ gverse/index.ts
@@ -0,0 +1,7 @@
+const sayHello = (name: string) => {
+	return `Hello, ${name}!`;
+};
+
+let user = [42, 9000, 7];
+
+console.log(sayHello(user));
