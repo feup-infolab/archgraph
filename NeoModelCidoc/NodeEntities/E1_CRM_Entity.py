c163257caa99c1ba6f6528b0fd7e8caed05d83d3
Nuno <nuno.freitas96@gmail.com>
2019-11-21 10:35:29 +0000

    Cardinality Tests

--- NeoModelCidoc/NodeEntities/E1_CRM_Entity.py
+++ NeoModelCidoc/NodeEntities/E1_CRM_Entity.py
@@ -1,7 +1,8 @@
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-                      UniqueIdProperty, RelationshipTo)
+                      UniqueIdProperty, RelationshipTo, One)
 
 
 class E1_CRM_Entity(StructuredNode):
     name = StringProperty(unique_index=True, required=True)
+    testCardinality = RelationshipTo('E1_CRM_Entity', 'Cardinality_Relationship', cardinality=One)
 
--- NeoModelCidoc/UnitTesting/test_relations.py
+++ NeoModelCidoc/UnitTesting/test_relations.py
@@ -6,12 +6,13 @@ from NodeEntities.E18_Physical_Thing import E18_Physical_Thing
 from NodeEntities.E24_Physical_Man_Made_Thing import E24_Physical_Man_Made_Thing
 from NodeEntities.E52_Time_Span import E52_Time_Span
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-                      UniqueIdProperty, RelationshipTo, OUTGOING, Traversal, DeflateError)
+                      UniqueIdProperty, RelationshipTo, OUTGOING, Traversal, DeflateError,
+                      AttemptedCardinalityViolation)
 
 config.DATABASE_URL = 'bolt://neo4j:password@localhost:7687'
 e1 = E1_CRM_Entity(name="test").save()
 e55 = E55_Type(name="test2").save()
-e552 = E55_Type(name="test3").save()
+e55_2 = E55_Type(name="test3").save()
 e18 = E18_Physical_Thing(name="e18").save()
 e18_2 = E18_Physical_Thing(name="e18_2").save()
 e24 = E24_Physical_Man_Made_Thing(name="e24").save()
@@ -21,7 +22,7 @@ class TestRelatioships(unittest.TestCase):
     def test_basic_relationship(self):
         # Tests creation of basic relationships
         e55.hasType.connect(e1)
-        e552.hasType.connect(e55)
+        e55_2.hasType.connect(e55)
         all_types = e55.hasType.filter(name="test2")
         for p in all_types:
             self.assertAlmostEqual(p.id, e1.id)
@@ -31,7 +32,7 @@ class TestRelatioships(unittest.TestCase):
         definition = dict(node_class=E55_Type, direction=OUTGOING, relation_type=None, model=None)
         relations_traversal = Traversal(e1, E55_Type.__label__, definition)
         all_relations = relations_traversal.all()
-        e55rels = e552.hasType
+        e55rels = e55_2.hasType
         for p in all_relations:
             self.assertAlmostEqual(p.id, e55.id)
         for p in e55rels:
@@ -61,6 +62,26 @@ class TestRelatioships(unittest.TestCase):
         returnede52 = E52_Time_Span.nodes.get(date=future_date)
         self.assertAlmostEqual(e52.id, returnede52.id)
         self.assertRaises(DeflateError, E52_Time_Span(name="e52", date="shouldn't work").save)
+
+    def test_expand_graph_3_levels(self):
+        # Test if traversals can be done for 3 levels
+        definition = dict(node_class=E1_CRM_Entity, direction=OUTGOING, relation_type=None, model=None)
+        relations_traversal = Traversal(e1, E1_CRM_Entity.__label__, definition)
+        all_relations = relations_traversal.all()
+        relations_traversal2 = Traversal(relations_traversal, E1_CRM_Entity.__label__, definition)
+        all_relations2 = relations_traversal2.all()
+        for p in all_relations:
+            self.assertAlmostEqual(p.id, e55.id)
+        for p in all_relations2:
+            self.assertAlmostEqual(p.id, e55_2.id)
+
+    def test_cardinality(self):
+        # Test if cardinality holds
+        e1_2 = E1_CRM_Entity(name="cardTest").save()
+        e24_2 = E24_Physical_Man_Made_Thing(name="test4").save()
+        e24_3 = E24_Physical_Man_Made_Thing(name="test5").save()
+        e1_2.testCardinality.connect(e24_2)
+        self.assertRaises(AttemptedCardinalityViolation,e1_2.testCardinality.connect,e24_3)
 
 
 
