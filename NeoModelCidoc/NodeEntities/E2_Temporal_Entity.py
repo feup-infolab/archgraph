c88631cf4aa7be40afec75db2bfeff7ca961a255
Nuno <nuno.freitas96@gmail.com>
2020-02-04 14:48:54 +0000

    Improvement of E2

--- NeoModelCidoc/NodeEntities/E2_Temporal_Entity.py
+++ NeoModelCidoc/NodeEntities/E2_Temporal_Entity.py
@@ -1,6 +1,11 @@
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
                       UniqueIdProperty, RelationshipTo, One)
 from NodeEntities.E1_CRM_Entity import E1_CRM_Entity
+from NodeProperties.StructuredRelCl import StructuredRelCl
+
+
+class P114_is_equal_in_time_to(StructuredRelCl):
+    pass
 
 
 class E2_Temporal_Entity(E1_CRM_Entity):
--- NeoModelCidoc/NodeEntities/E52_Time_Span.py
+++ NeoModelCidoc/NodeEntities/E52_Time_Span.py
@@ -1,7 +1,14 @@
 from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
-                      UniqueIdProperty, RelationshipTo, DateTimeProperty)
+                      UniqueIdProperty, RelationshipTo, DateTimeProperty, RelationshipFrom)
 from NodeEntities.E1_CRM_Entity import E1_CRM_Entity
+from NodeEntities.E2_Temporal_Entity import E2_Temporal_Entity
+from NodeProperties.StructuredRelCl import StructuredRelCl
+
+
+class P4_has_time_span(StructuredRelCl):
+    pass
 
 
 class E52_Time_Span(E1_CRM_Entity):
     date = DateTimeProperty(unique_index=True, required=True)
+    hasTimeSpan = RelationshipFrom(E2_Temporal_Entity, 'P4_has_time_span', model=P4_has_time_span)
