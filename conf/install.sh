1f9c92877af4ec910718cb3f6379eddb43843194
João Rocha da Silva <joaorosilva@gmail.com>
2020-02-14 21:15:33 +0000

    Fixed some bugs in install and run scripts when in Ubuntu

--- conf/install.sh
+++ conf/install.sh
@@ -5,15 +5,20 @@ if [ "$(uname)" == "Darwin" ]; then
     brew cask install miniconda || brew cask upgrade miniconda
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     # Do something under GNU/Linux platform
+    rm -rf Miniconda3-latest-Linux-x86_64.sh
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
     chmod +x Miniconda3-latest-Linux-x86_64.sh
-    ./Miniconda3-latest-Linux-x86_64.sh
+    ./Miniconda3-latest-Linux-x86_64.sh -b -p "$HOME/miniconda"
+    rm -rf Miniconda3-latest-Linux-x86_64.sh
 fi
 
+
+# run conda
+source ~/.bash_profile
+export PATH="$HOME/miniconda/bin":$PATH
 conda create -y -n archgraph python=3.7 anaconda
 conda activate archgraph
-conda init
-source ~/.bash_profile
+conda init bash
 
 # install pip
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
@@ -24,6 +29,24 @@ pip install --upgrade pip
 pip install -r requirements.txt
 # Get location of python interpreter
 echo "Python interpreter is at: ---> $(which python) <---"
+
+# install nodejs and yarn
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+
+# activate nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+#install nodejs and yarn
+nvm install v13
+nvm use v13
+npm install -g yarn
+
+
+# run yarn install
+cd frontend
+yarn install
+cd -
 
 exit 0
 
--- conf/run.sh
+++ conf/run.sh
@@ -2,6 +2,17 @@
 ROOT_DIR=$(pwd)
 echo "Running at $ROOT_DIR"
 
+# maybe remove this later when conda is working correctly in path
+# (this was not working on linux without re-exporting the folder to path?)
+
+source "$HOME/.bash_profile"
+export PATH="$HOME/miniconda/bin":$PATH
+
+# activate nvm and use node v13
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm use v13
+
 eval "$(conda shell.bash hook)"
 conda activate archgraph
 echo "Python interpreter is at: ---> $(which python) <---"
