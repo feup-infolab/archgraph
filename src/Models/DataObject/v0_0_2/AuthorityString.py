bace0b0c9440e2bf2f0430494a8775b6ae59f0d0
lazaro <lazaroosta@hotmail.com>
2020-02-20 15:09:26 +0000

    add files

---
+++ src/Models/DataObject/v0_0_2/AuthorityString.py
@@ -0,0 +1,8 @@
+from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
+                      UniqueIdProperty, RelationshipTo, RelationshipFrom)
+from json import JSONEncoder
+from DataObject.String import String
+
+
+class AuthorityString(String):
+    pass
---
+++ src/Models/DataObject/v0_0_2/Instant.py
@@ -0,0 +1,13 @@
+from neomodel import (config, StructuredNode, StringProperty, IntegerProperty,
+                      UniqueIdProperty, RelationshipTo,One,  RelationshipFrom, One)
+from json import JSONEncoder
+from DataObject.Date import Date
+from NodeProperties.StructuredRelCl import StructuredRelCl
+
+
+class xsdDateTime(StructuredRelCl):
+    pass
+
+
+class Instant(Date):
+    timestamp = RelationshipFrom('Instant', 'xsdDateTime', cardinality=One, model=xsdDateTime)
