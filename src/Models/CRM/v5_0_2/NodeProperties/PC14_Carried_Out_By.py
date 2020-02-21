d62fc035bf073539b938ea65d0a7f09498d9d91b
João Rocha da Silva <joaorosilva@gmail.com>
2020-02-21 15:38:24 +0000

    Updated clean_code.sh to optimize imports

--- src/Models/CRM/v5_0_2/NodeProperties/PC14_Carried_Out_By.py
+++ src/Models/CRM/v5_0_2/NodeProperties/PC14_Carried_Out_By.py
@@ -7,7 +7,7 @@ import src.Models.CRM.v5_0_2.NodeProperties.PC0_CRM_Property as PC0_CRM_Property
 class PC14_Carried_Out_By(PC0_CRM_Property):
     inTheRoleOf = RelationshipTo(
         "..NodeEntities.E55_Type.E55_Type",
-        "P14.1_in_the_role_of",
+        ".P14.1_in_the_role_of",
         cardinality=One,
-        model=P14_1_in_the_role_of,
+        model=P14_1_in_the_role_of
     )
