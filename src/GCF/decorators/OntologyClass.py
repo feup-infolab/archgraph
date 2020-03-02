302997a98ddcadbfc5c3557bbc602025e9ab8974
João Rocha da Silva <joaorosilva@gmail.com>
2020-03-02 16:27:17 +0000

    trying to add tojson method to instance classes via decorator

--- src/GCF/decorators/OntologyClass.py
+++ src/GCF/decorators/OntologyClass.py
@@ -1,4 +1,19 @@
 import importlib
+from marshmallow import Schema, fields
+from marshmallow_jsonschema import JSONSchema
+from functools import wraps # This convenience func preserves name and docstring
+
+def has_json_schema(cls):
+
+    def getSchema(self):
+        string_schema = StringSchema()
+        json_schema = JSONSchema()
+        return json_schema.dump(string_schema)
+
+    # package_name = ".".join([cls.__module__, cls.__name__])
+    package_name = ".".join(str.split(cls.__module__, ".")[:-2])
+    module = importlib.import_module(cls.__module__ + "Schema")
+
 
 
 def ontology_class(cls):
--- src/Models/DataObject/v0_0_2/String.py
+++ src/Models/DataObject/v0_0_2/String.py
@@ -1,22 +1,16 @@
 import json
-
 from marshmallow import Schema, fields
-from marshmallow_jsonschema import JSONSchema
+
 from neomodel import StringProperty
 from src.Models.DataObject.v0_0_2.DataObject import DataObject
 
 
+@has_json_schema
 class String(DataObject):
     stringValue = StringProperty(unique_index=True, required=True)
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__)
-
-    def getSchema(self):
-        string_schema = StringSchema()
-        json_schema = JSONSchema()
-        return json_schema.dump(string_schema)
-
 
 class StringSchema(Schema):
     stringValue = fields.String(required=True)
