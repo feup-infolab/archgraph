bce87c1a3594b41ef9733384649bbfd7b60540a9
Nuno <nuno.freitas96@gmail.com>
2020-03-31 22:42:18 +0100

    Full text Search with Labels

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { MyServiceService } from './service/my-service.service';
 import {Bootstrap4FrameworkModule, MaterialDesignFrameworkModule} from 'angular7-json-schema-form';
 import { AppComponent } from './app.component';
@@ -9,11 +9,14 @@ import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-b
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {ComboBoxComponent} from './combo-box/combo-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExampleComponent
+    ExampleComponent,
+    ComboBoxComponent,
+    ComboBoxComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import {FormsModule} from '@angular/forms';
     FlexLayoutModule
   ],
   providers: [MyServiceService],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
 
